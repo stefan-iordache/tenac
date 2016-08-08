a8f8ed29220dd0898bf38e92503d1edae857099e
stefan-iordache <stefaniordache472@gmail.com>
2016-08-08 12:16:51 +0300

    My first commit

---
+++ config-sample.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * THIS FILE SHOULD BE CHANGED TO "config.php" WHEN USED IN PRACTICE
+ */
+
+/*
+ * Sets the default timezone used by all date/time functions in a script
+ * Available time zones: http://php.net/manual/en/timezones.php
+ */
+// It may be useful in the future
+//date_default_timezone_set('time_zone_here');
+
+define('TENAC_DEBUG_ON', false);
+
+/** MySQL database name */
+define('DB_NAME', 'db_name_here');
+
+/** MySQL database username */
+define('DB_USER', 'username_here');
+
+/** MySQL database user password */
+define('DB_PASSWD', 'db_pass_here');
